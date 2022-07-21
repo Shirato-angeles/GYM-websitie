f4abaed345248f9476b7e8b3c662a201ef7201d3
miguel <miguel.hurtado@onewayecommerce.com>
2022-07-21 13:33:42 -0500

    more

--- my-app/src/components/Hero/Hero.jsx
+++ my-app/src/components/Hero/Hero.jsx
@@ -6,11 +6,49 @@ const Hero = () => {
     <div className="hero">
         <div className="left-h">
             <Header/>
-
+        {/* the best add */}
             <div className="the-best-add">
                 <div></div>
                 <span>The best fitness club in the town</span>
             </div>
+        {/* Hero heading*/}
+        <div className="hero-text">
+            <div>
+                <span className="stroke-text">shape </span>
+                <span>your</span>
+            </div>
+            <div>
+            <span> ideal body</span>
+            </div>
+            <div>
+                <span>In here we will help you to shape and build your 
+                ideal body and live up your life to fullest</span>
+            </div>
+        </div>
+
+        {/*figures */}
+        <div className="figures">
+            <div>
+                <span>+ 140</span>
+                <span>EXPERT COACHES</span>
+            </div>
+            <div>
+                <span>+ 978</span>
+                <span>MEMBERS JOINED</span>
+            </div>
+            <div>
+                <span>+ 50</span>
+                <span>FITNESS PROGRAMS</span>
+            </div>
+        </div>
+
+        {/*hero buttons */}
+
+        <div className="hero-button">
+            <button  className="btn">Get Started</button>
+            <button  className="btn">Learn More</button>
+
+        </div>
         </div>
         <div className="rifht-h">right side</div>
     </div>
