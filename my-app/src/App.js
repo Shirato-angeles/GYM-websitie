a0c1d4f3067a85dab24d764d5a6bc9ca3d5af89e
miguel <miguel.hurtado@onewayecommerce.com>
2022-07-22 11:51:38 -0500

    reasons

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,14 @@
 import Hero from './components/Hero/Hero';
 import Programs from './components/Programas/Programs'
 import './App.css';
-import why_us from './components/Reasons/why_us';
+import Reasons from './components/Reasons/Reasons';
 
 function App() {
   return (
     <div className="App">
       <Hero/>
       <Programs/>
-      <why_us/>
+      <Reasons/>
     </div>
   );
 }
--- my-app/src/components/Reasons/Reasons.jsx
+++ my-app/src/components/Reasons/Reasons.jsx
@@ -1,10 +1,62 @@
 import React from 'react'
-import './why_us.css'
+import './reasons.css'
+import image1 from '../../assets/image1.png'
+import image2 from '../../assets/image2.png'
+import image3 from '../../assets/image3.png'
+import image4 from '../../assets/image4.png'
+import nb from '../../assets/nb.png'
+import adidas from '../../assets/adidas.png'
+import nike from '../../assets/nike.png'
+import tick from '../../assets/tick.png'
 
-const why_us = () => {
+const Reasons = () => {
   return (
-    <div>why_us</div>
+    <div className="reasons">
+      <div className="left-r">
+          <img src={image1} />
+          <img src={image2} />
+          <img src={image3} />
+          <img src={image4} />
+
+      </div>
+      <div className="right-r">
+        <span>SOME REASONS</span>
+        <div className="">
+        <span className="stroke-text">WHY</span>
+        <span className=""> CHOOSE US?</span>
+      </div>
+
+      <div className="details-r">
+          <div>
+            <img src={tick} />
+            <span> OVER 140+ EXPERT COACHS</span>
+          </div>
+          <div>
+            <img src={tick} />
+            <span> TRAIN SMARTER AND FASTER THAN BEFORE</span>
+          </div> <div>
+            <img src={tick} />
+            <span> 1 FREE PROGRAM FOR NEW MEMBER</span>
+          </div> <div>
+            <img src={tick} />
+            <span> RELIABLE PARTNERS</span>
+          </div>
+      </div>
+      <span
+        style= {{
+          color: 'var(--gary)',
+          fontweight: 'normal'
+      }}>OUR PARTNERS</span>
+
+      <div className="partners">
+        <img src={nb} />
+        <img src={adidas} />
+        <img src={nike} />
+
+      </div>
+      </div>
+    </div>
   )
 }
 
-export default why_us
+export default Reasons
